5f0cf03b8c7bccdb9e7ebf390d78f171ea4544b2
Filippo Martino <43878495+FilippoMartino@users.noreply.github.com>
2022-12-12 15:20:37 +0100

    implementato sistema di conversione in c

--- LAB09/ES1/button_EXINT/IRQ_button.c
+++ LAB09/ES1/button_EXINT/IRQ_button.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "button.h"
 #include "lpc17xx.h"
 
 #include "../led/led.h"
-
+extern void c_led_setup(void);
 /*
 	Questo file permette la configurazione degli handler per gli interrupt causati
 	dai pulsanti, o meglio dalla loro attivazione.
@@ -17,17 +19,24 @@
 	specificando il registro EXTINT in bitwise AND con la maschera per selezionare a quale pin di input si stia specificando, 
 	in questo caso 0, 1, o 2 in base al pulsante
 */
+
 void EINT0_IRQHandler (void)	  
 {
-	LED_On(0);
+
   LPC_SC->EXTINT &= (1 << 0);     /* clear pending interrupt         */
 }
 
 
+/* 
+	Al trigger del KEY1 si inizia la lettura del messaggio in c: 
+	tutti i led vengono spenti e, ad ogni cambio di simbolo, i led
+	devono mostrare il numero di simboli letti fino a quel punto
+*/
 void EINT1_IRQHandler (void)	  
 {
-  LED_On(1);
+  /* la funzione LED_Out assegna a tutti i led il valore in argomento */
 	LPC_SC->EXTINT &= (1 << 1);     /* clear pending interrupt         */
+	c_led_setup();
 }
 
 void EINT2_IRQHandler (void)	  
--- LAB09/ES1/sample.c
+++ LAB09/ES1/sample.c
@@ -1,5 +1,3 @@
-
-                  
 #include <stdio.h>
 #include "LPC17xx.H"                    /* LPC17xx definitions                */
 #include "led/led.h"
@@ -14,13 +12,22 @@ extern int translate_morse(char* vett_input,
 										char space, 
 										char sentence_end);
 
-
+/*
+	Funzione per la conversione del vettore in input 
+*/
+void c_led_setup(void);
+/* Funzione per la conversione decimale-binario */
+void convert(int to_convert);
+										
 /* Led external variables from funct_led */
 extern unsigned char led_value;					/* defined in funct_led								*/
 #ifdef SIMULATOR
 extern uint8_t ScaleFlag; // <- ScaleFlag needs to visible in order for the emulator to find the symbol (can be placed also inside system_LPC17xx.h but since it is RO, it needs more work)
 #endif
 
+/* vettore dichiarato globale */										
+char	vett_input[100] = {'0', '0', '0', '0', '2', '1', '1', '1', '2', '0', '1', '0', '0', '2', '0', '1', '3', '1', '1', '2', '0', '0', '1', '2', '1', '0', '2', '1', '0', '0', '2', '1', '1', '1', '3', '0', '1', '1', '1', '1', '2', '0', '0', '1', '1', '1', '4'};	
+										
 /*----------------------------------------------------------------------------
   Main Program
  *----------------------------------------------------------------------------*/
@@ -28,16 +35,63 @@ int main (void) {
   
 	/* definizione variabili */
 	/* int 	vett_input[100] 	= {0, 0, 0, 0, 2, 1, 1, 1, 2, 0, 1, 0, 0, 2, 0, 1, 3, 1, 1, 2, 0, 0, 1, 2, 1, 0, 2, 1, 0, 0, 2, 1, 1, 1, 3, 0, 1, 1, 1, 1, 2, 0, 0, 1, 1, 1, 4}; */
-  char	vett_input[100] = {'0', '0', '0', '0', '2', '1', '1', '1', '2', '0', '1', '0', '0', '2', '0', '1', '3', '1', '1', '2', '0', '0', '1', '2', '1', '0', '2', '1', '0', '0', '2', '1', '1', '1', '3', '0', '1', '1', '1', '1', '2', '0', '0', '1', '1', '1', '4'}; 
+
 	char	vett_output[100];
 	char	change_symbol 		= '2';
 	char	space							= '3';
 	char	sentence_end			= '4';
 	
+	SystemInit();
+	LED_init();
+	BUTTON_init();
 	
+	//translate_morse((char*) vett_input, 100, vett_output, 100, change_symbol, space, sentence_end);
+
+	while(1);
+
+}
+
+
+
+void c_led_setup(void){
 	
-	translate_morse((char*) vett_input, 100, vett_output, 100, change_symbol, space, sentence_end);
+	/* variabile conteggio caratteri morse */
+	int count = 0;
+	int i;
+	
+	/* ciclo per ogni valore morse presente nel vettore */
+	for (i = 0; i < 100; i++){
+		if (vett_input[i] == '2' || vett_input[i] == '3'){
+			count ++;
+			convert(count);
+		}
+		if (vett_input[i] == '4'){
+			count ++;
+			convert(count);
+			break;
+		}
+	}
+	return;
+}
 
-	return 0;
+void convert(int to_convert){
 
+	int i;
+	/* vettore in cui salvare numero binario (8 bit per 8 led) */
+	int bin[8];
+	
+	/* Spengo tutti i led */
+	LED_Out(0);
+	for (i = 0; i < 8; i++){
+		bin[i] = to_convert % 2;
+		to_convert /= 2;
+	}
+
+	/* Accendiamo i led a seconda del valore binario */
+	for (i = 0; i < 8; i++){
+		if (bin[i])
+			LED_On(i);
+	}
+	
+	return;
 }
